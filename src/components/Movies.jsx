dd42bed2a3c87648e7d7181047b4ef41e2cf3b34
sameerdiwse <sameerdiwse9090@gmail.com>
2025-04-22 20:21:11 +0530

    Created Movie Cards

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-
-function Movies()
-{
-    return(
+function Movies() {
+    return (
         <>
-       <h1>These are the Movies</h1> 
-        </>
+            <div className="flex">
+            <div className="m-4 rounded w-[100px] h-[35vh] bg-cover bg-center" style={{backgroundImage: `url(https://legacymovieposters.com/cdn/shop/products/IronMan3_photo.jpg)`}}>
+            </div>
+ <div className="m-4 rounded w-[100px] h-[35vh] bg-cover bg-center" style={{backgroundImage: `url(https://legacymovieposters.com/cdn/shop/products/IronMan3_photo.jpg)`}}>
+            </div>
+             <div className="m-4 rounded w-[100px] h-[35vh] bg-cover bg-center" style={{backgroundImage: `url(https://legacymovieposters.com/cdn/shop/products/IronMan3_photo.jpg)`}}>
+            </div>
+ 
+            </div>
+       </>
     )
 }
 
